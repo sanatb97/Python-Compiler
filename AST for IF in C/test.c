90a6aa6cf7676c538cbeec0ed4d6af4372cb5ae6
Sailesh Gaddalay <33772198+sailesh-gaddalay@users.noreply.github.com>
2019-04-21 16:15:20 +0530

    Add files via upload

---
+++ AST for IF in C/test.c
@@ -0,0 +1,26 @@
+#include "test.h"
+#include <stdio.h>
+#include <stdlib.h>
+struct s {
+	int p;
+};
+int func(int r,int w)
+	{
+		int c=r+w;
+	}
+int main () 
+{	
+	int a,b,c,d,i,arr[10];
+	int k[2],sum;
+	float y[100],x=4.0;
+	x=x+1;
+	if(a==4)
+	{
+		d=2;
+	}
+	else
+	{
+		a=a+3;
+	}
+	return 0;
+} 
